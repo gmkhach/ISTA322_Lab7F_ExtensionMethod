884a66f2c07e448d9cf741cad964e5ceaeba9329
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-23 21:45:07 -0400

    Add project files.

---
+++ ExtensionMethod/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extensions
+{
+    static class Util
+    {
+        public static int ConvertToBase(this int i, int baseToConvertTo)
+        {
+            if (baseToConvertTo < 2 || baseToConvertTo > 10)
+            {
+                throw new ArgumentException("Value cannot be converted to base " +
+                baseToConvertTo.ToString());
+            }
+        }
+    }
+}
